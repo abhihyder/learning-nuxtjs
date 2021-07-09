9f244c7a0f2ad33eab7bf2f9078d65bfea0e2fe3
Tofayel Hyder <abhihyder7@gmail.com>
2021-07-09 15:59:32 +0600

    making weather app

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-container class="bv-example-row">
+        <b-navbar-brand href="javascript:;">Weather App</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#"> hello</b-nav-item>
+            <b-nav-item href="#"> hey</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto mr-2">
+            <b-nav-form>
+              <b-form-input
+                size="sm"
+                class="mr-sm-2"
+                placeholder="Search location"
+              ></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                >Search</b-button
+              >
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <b-container class="bv-example-row mt-4">
+      <div class="d-flex justify-content-center mt-3">
+        <div style="max-width: 20rem" class="mb-2">
+          <b-input-group class="mt-3">
+            <b-form-input
+              v-model="location"
+              placeholder="Search location"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button variant="info" @click="searchWeatherInfo"
+                >Search</b-button
+              >
+            </b-input-group-append>
+          </b-input-group>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center mt-3">
+        <b-card
+          title="Card Title"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      location: 'Dhaka',
+      weatherInfo: {},
+    }
+  },
+  created() {
+    this.searchWeatherInfo()
+  },
+  methods: {
+    searchWeatherInfo() {
+      this.$axios
+        .$get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${this.location}&appid=${process.env.weatherApiKey}`
+        )
+        .then((response) => {
+          this.weatherInfo = response
+        })
+    },
+  },
+}
+</script>
